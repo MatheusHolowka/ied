106b44a1010cfb02a614a079fd894a8324361b5e
Matheus Holowka <106926519+MatheusHolowka@users.noreply.github.com>
2024-06-21 20:37:53 -0400

    Update Scripts .sql

--- Scripts .sql
+++ Scripts .sql
@@ -1,33 +1,39 @@
-1- Quantidade de produtos vendidos por categoria
-SELECT c.nome AS categoria, SUM(pp.quantidade) AS quantidade_vendida
-FROM relacional.pedido_produto pp
-JOIN relacional.produto p ON pp.produto_codigo = p.codigo
-JOIN relacional.categoria c ON p.categoria_codigo = c.codigo
-GROUP BY c.nome;
+1 - Quantidade de produtos vendidos por categoria.
 
-2- Quantidade de clientes considerado BONS
-SELECT COUNT(*) AS clientes_bons
-FROM relacional.cliente
+SELECT dc.nome AS categoria, SUM(fv.quantidade) AS quantidade_vendida
+FROM Dimensional.fato_vendas fv
+JOIN Dimensional.dim_produto dp ON fv.produto_id = dp.produto_id
+JOIN Dimensional.dim_categoria dc ON dp.categoria_id = dc.categoria_id
+GROUP BY dc.nome
+ORDER BY quantidade_vendida DESC;
+
+2 - Quantidade de clientes considerado BONS
+SELECT COUNT(*) AS quantidade_clientes_bons
+FROM Dimensional.dim_cliente
 WHERE status = 'bom';
 
-3- Quantos Pedidos foram realizados no mês de maio
-SELECT COUNT(*) AS pedidos_maio
-FROM relacional.pedido
-WHERE EXTRACT(MONTH FROM data) = 5;
+
+3 - Quantos Pedidos foram realizados no mês de maio
+SELECT COUNT(DISTINCT fv.venda_id) AS quantidade_pedidos_maio
+FROM Dimensional.fato_vendas fv
+JOIN Dimensional.dim_tempo dt ON fv.data = dt.data
+WHERE dt.mes = 5;
+
+
 
 4- Qual produto foi mais vendido no mês de julho:
-SELECT p.nome AS produto_mais_vendido, SUM(pp.quantidade) AS quantidade_vendida
-FROM relacional.pedido_produto pp
-JOIN relacional.produto p ON pp.produto_codigo = p.codigo
-JOIN relacional.pedido ped ON pp.pedido_numero = ped.numero
-WHERE EXTRACT(MONTH FROM ped.data) = 7
-GROUP BY p.nome
+SELECT dp.nome AS produto, SUM(fv.quantidade) AS quantidade_vendida
+FROM Dimensional.fato_vendas fv
+JOIN Dimensional.dim_produto dp ON fv.produto_id = dp.produto_id
+JOIN Dimensional.dim_tempo dt ON fv.data = dt.data
+WHERE dt.mes = 7
+GROUP BY dp.nome
 ORDER BY quantidade_vendida DESC
 LIMIT 1;
 
 5- Qual o valor total de vendas no mês de junho:
-SELECT SUM(p.preco * pp.quantidade) AS valor_total_vendas_junho
-FROM relacional.pedido_produto pp
-JOIN relacional.produto p ON pp.produto_codigo = p.codigo
-JOIN relacional.pedido ped ON pp.pedido_numero = ped.numero
-WHERE EXTRACT(MONTH FROM ped.data) = 6;
+SELECT SUM(fv.valor_total) AS valor_total_vendas_junho
+FROM Dimensional.fato_vendas fv
+JOIN Dimensional.dim_tempo dt ON fv.data = dt.data
+WHERE dt.mes = 6;
+
